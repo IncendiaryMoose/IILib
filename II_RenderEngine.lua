c7e193d50d6ec5efa0d2c60df52c0871fcce33b2
IncendiaryMoose <Ian.Okiishi@gmail.com>
2023-06-07 15:46:29 -0600

    Added triangle format to render engine

--- II_RenderEngine.lua
+++ II_RenderEngine.lua
@@ -6,10 +6,29 @@ h = 160
 w = 288
 monitor = {meterToPixelW = w/2.25, meterToPixelH = h/1.25, pixelW = w, pixelH = h, meterW = 2.25, meterH = 1.25, offsetX = 0, offsetY = 0, centerW = w/2-1, centerH = h/2, ratio = w/h}
 
-drawTri = function (tri)
-    if tri.p1 and tri.p2 and tri.p3 then
-        screen.drawTriangleF(tri.p1.x, tri.p1.y, tri.p2.x, tri.p2.y, tri.p3.x, tri.p3.y)
+currentColor = {0, 0, 0}
+setDrawColor = function (color)
+    if currentColor[1] ~= color[1] or currentColor[2] ~= color[2] or currentColor[3] ~= color[3] then
+        currentColor[1] = color[1]
+        currentColor[2] = color[2]
+        currentColor[3] = color[3]
+        screen.setColor(color[1], color[2], color[3])
     end
+end
+
+newTriangle = function (a, b, c, color)
+    return {
+        {a[1], a[2]},
+        {b[1], b[2]},
+        {c[1], c[2]},
+        (a[3] + b[3] + c[3]) / 3,
+        color
+    }
+end
+
+drawTri = function (tri)
+    setDrawColor(tri[5])
+    screen.drawTriangleF(tri[1][1], tri[1][2], tri[2][1], tri[2][2], tri[3][1], tri[3][2])
 end
 
 worldToScreenPoint = function(cameraLocation, cameraMatrix, points, screenPoints)
